0fac91d5b95d90933c79ffb2eda69d000e1759d8
charleeo <charleeotaru@gmail.com>
2024-02-24 20:39:21 +0100

    Testing

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,11 @@
+
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.21.0"
-    }
-    
-  }
-  
-backend "remote" {
+  backend "remote" {
     organization = "charleeo"
-    workspace {
+    workspaces {
       name = "terraform-learn-circleci"
     }
-}
-  required_version = "~> 1.2"
+  }
 }
 
 
