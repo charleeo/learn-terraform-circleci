e30fa259deb685deaa101785c5cd28b203c74cf7
charleeo <charleeotaru@gmail.com>
2024-02-24 20:26:29 +0100

    Testing

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.21.0"
     }
+    
   }
-
+  
+backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "charleeo"
+    workspaces {
+      name = "my-app-prod"
+    }
+}
   required_version = "~> 1.2"
 }
